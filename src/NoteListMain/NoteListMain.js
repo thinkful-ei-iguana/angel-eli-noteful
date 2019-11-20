00d1b62f028e32d9ceedd07b5c286c2458539ec3
jpegzilla <eris@jpegzilla.com>
2019-11-20 11:10:40 -0500

    added context

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,40 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Note from '../Note/Note'
 import CircleButton from '../CircleButton/CircleButton'
 import './NoteListMain.css'
+import context from './../context'
 
-export default function NoteListMain(props) {
-  return (
-    <section className='NoteListMain'>
+export default class NoteListMain extends Component {
+
+  static contextType = context;
+
+  render() {
+    const { notes } = this.context
+
+    return ( <section className='NoteListMain'>
       <ul>
-        {props.notes.map(note =>
-          <li key={note.id}>
-            <Note
-              id={note.id}
-              name={note.name}
-              modified={note.modified}
-            />
-          </li>
-        )}
+        {
+          notes.map( note => <li key={note.id}>
+            <Note id={note.id} name={note.name} modified={note.modified}/>
+          </li> )
+        }
       </ul>
       <div className='NoteListMain__button-container'>
-        <CircleButton
-          tag={Link}
-          to='/add-note'
-          type='button'
-          className='NoteListMain__add-note-button'
-        >
-          <FontAwesomeIcon icon='plus' />
-          <br />
+        <CircleButton tag={Link} to='/add-note' type='button' className='NoteListMain__add-note-button'>
+          <FontAwesomeIcon icon='plus'/>
+          <br/>
           Note
         </CircleButton>
       </div>
-    </section>
-  )
+    </section> )
+  }
 }
 
 NoteListMain.defaultProps = {
-  notes: [],
+  notes: []
 }
